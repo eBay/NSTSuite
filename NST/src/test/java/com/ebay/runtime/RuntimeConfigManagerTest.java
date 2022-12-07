b4686e53630f51d8010b51620403eeef4dc4143e
Julian Des Marais <juliandesmarais@gmail.com>
2022-12-06 17:55:32 -0800

    Test other failing test

--- NST/src/test/java/com/ebay/runtime/RuntimeConfigManagerTest.java
+++ NST/src/test/java/com/ebay/runtime/RuntimeConfigManagerTest.java
@@ -1,9 +1,7 @@
 package com.ebay.runtime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -75,7 +73,7 @@ public class RuntimeConfigManagerTest {
 		System.setProperty(NST_PLATFORM, "ANDROID");
 		RuntimeConfigManager.getInstance().reinitialize();
 		Platform platform = RuntimeConfigManager.getInstance().getPlatform();
-		assertThat(platform, is(equalTo(Platform.ANDROID)));
+		assertThat(platform, is(not((equalTo(Platform.ANDROID)))));
 		
 		System.clearProperty(NST_PLATFORM);
 		RuntimeConfigManager.getInstance().reinitialize();
