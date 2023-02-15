847589c613a4c73eeb322528658471a28871c4cc
Ben Yarger <byarger@ebay.com>
2023-02-14 17:46:38 -0800

    Adding interface for developer mock generic type decoding.

---
+++ TMBuilder/src/test/java/com/ebay/tool/thinmodelgen/gui/menu/export/DeveloperMockTypeDecoderTest.java
@@ -0,0 +1,73 @@
+package com.ebay.tool.thinmodelgen.gui.menu.export;
+
+import com.ebay.service.protocol.http.NSTHttpResponse;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.*;
+
+public class DeveloperMockTypeDecoderTest {
+
+    @Test
+    public void getBooleanMockTypeFromGeneric() {
+        BooleanDeveloperMockType booleanMockType = new BooleanDeveloperMockType();
+        DeveloperMockType mockType = booleanMockType.getMockType();
+        assertThat(mockType, is(equalTo(DeveloperMockType.BOOLEAN)));
+    }
+
+    @Test
+    public void getDoubleMockTypeFromGeneric() {
+        DoubleDeveloperMockType doubleMockType = new DoubleDeveloperMockType();
+        DeveloperMockType mockType = doubleMockType.getMockType();
+        assertThat(mockType, is(equalTo(DeveloperMockType.DOUBLE)));
+    }
+
+    @Test
+    public void getIntegerMockTypeFromGeneric() {
+        IntegerDeveloperMockType intMockType = new IntegerDeveloperMockType();
+        DeveloperMockType mockType = intMockType.getMockType();
+        assertThat(mockType, is(equalTo(DeveloperMockType.INTEGER)));
+    }
+
+    @Test
+    public void getStringMockTypeFromGeneric() {
+        StringDeveloperMockType stringMockType = new StringDeveloperMockType();
+        DeveloperMockType mockType = stringMockType.getMockType();
+        assertThat(mockType, is(equalTo(DeveloperMockType.STRING)));
+    }
+
+    @Test
+    public void getUnknownMockTypeFromGeneric() {
+        UnknownDeveloperMockType unknownDeveloperMockType = new UnknownDeveloperMockType();
+        DeveloperMockType mockType = unknownDeveloperMockType.getMockType();
+        assertThat(mockType, is(equalTo(DeveloperMockType.UNKNOWN)));
+    }
+
+    class BooleanDeveloperMockType implements DeveloperMockTypeDecoder<Boolean> {
+
+    }
+
+    class DoubleDeveloperMockType implements DeveloperMockTypeDecoder<Double> {
+
+    }
+
+    class IntegerDeveloperMockType implements DeveloperMockTypeDecoder<Integer> {
+
+    }
+
+    class StringDeveloperMockType implements DeveloperMockTypeDecoder<String> {
+
+    }
+
+    class UnknownDeveloperMockType implements DeveloperMockTypeDecoder<Object> {
+
+    }
+}
