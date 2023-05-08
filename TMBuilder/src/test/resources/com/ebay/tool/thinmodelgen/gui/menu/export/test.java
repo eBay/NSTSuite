388e8aa6518383510c7513a59f1bf0d2836adb63
Shreyas Muthkur <smuthkur@ebay.com>
2023-05-08 02:28:06 -0400

    Added indentations based on tabs and not space

--- TMBuilder/src/test/resources/com/ebay/tool/thinmodelgen/gui/menu/export/test.java
+++ TMBuilder/src/test/resources/com/ebay/tool/thinmodelgen/gui/menu/export/test.java
@@ -13,20 +13,20 @@ import java.util.HashMap;
 
 public class Test extends ServiceModelBase {
 
-  public Test(JSONObject jsonRoot, SoftAssert softAssert) {
-    super(jsonRoot, softAssert);
-  }
+    public Test(JSONObject jsonObject, SoftAssert softAssert) {
+        super(jsonObject, softAssert);
+    }
 
-  @Override
-  protected void validate(SoftAssert softAssert) {
-		generatedValidations(softAssert);
-  }
+    @Override
+    protected void validate(SoftAssert softAssert) {
+        generatedValidations(softAssert);
+    }
 
-	// TMB Generated Validation Method
-	private void generatedValidations(SoftAssert softAssert) {
+    // TMB Generated Validation Method
+    private void generatedValidations(SoftAssert softAssert) {
 
-		Map<String, JsonPathExecutor> validations = new HashMap<>();
-		validations.put("$.modules.banners._type", new JPStringCheck());
-		evaluateJsonPaths(validations, softAssert);
-	}
+        Map<String, JsonPathExecutor> validations = new HashMap<>();
+        validations.put("$.modules.banners._type", new JPStringCheck());
+        evaluateJsonPaths(validations, softAssert);
+    }
 }
