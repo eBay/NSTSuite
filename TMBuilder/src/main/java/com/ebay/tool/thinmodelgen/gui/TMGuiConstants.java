9da32d9bfd4048de2f5526469f4d657a8e3ddf84
Ben Yarger <byarger@ebay.com>
2023-05-11 14:06:04 -0700

    v3.1.1 update with fix for developer mock export dialog.

--- TMBuilder/src/main/java/com/ebay/tool/thinmodelgen/gui/TMGuiConstants.java
+++ TMBuilder/src/main/java/com/ebay/tool/thinmodelgen/gui/TMGuiConstants.java
@@ -2,7 +2,7 @@ package com.ebay.tool.thinmodelgen.gui;
 
 public class TMGuiConstants {
 
-  public static final String APP_NAME = "TMBuilder v3.1.0";
+  public static final String APP_NAME = "TMBuilder v3.1.1";
   public static final int DEFAULT_WINDOW_WIDTH = 1000;
   public static final int DEFAULT_WINDOW_HEIGHT = 800;
   public static final String DICTIONARY_KEY = "<key>";
